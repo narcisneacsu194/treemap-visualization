1edca858af8cca50700f1fc8c67c67c79cca4b6f
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-03-20 12:54:19 +0200

    Almost finished on updating the treemap using different data sets. Some bugs left.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 let videoGameData;
+let movieData;
 let treemap;
 
-d3.json("data/videogames.json", function(error, data) {
+d3.json("data/videogames.json", function (error, data) {
   if (error) throw error;
-    videoGameData = data;
-    treemap = new Treemap();
+  convertChildValuesToNumbers(data);
+  videoGameData = data;
+  treemap = new Treemap();
+});
+
+d3.json('data/movies.json', function (error, data) {
+  if (error) throw error;
+
+  convertChildValuesToNumbers(data);
+
+  movieData = data;
+});
+
+function convertChildValuesToNumbers(data) {
+  data.children.forEach((d) => {
+    d.children.forEach((d2) => {
+      d2.value = +d2.value;
+      return d2;
+    });
   });
+};
--- js/treemap.js
+++ js/treemap.js
@@ -1,10 +1,10 @@
-Treemap = function(_parentElement) {
+Treemap = function (_parentElement) {
   this.parentElement = _parentElement;
 
   this.initVis();
 };
 
-Treemap.prototype.initVis = function() {
+Treemap.prototype.initVis = function () {
   let vis = this;
 
   vis.svg = d3.select("svg");
@@ -25,7 +25,7 @@ Treemap.prototype.initVis = function() {
   vis.tip = d3
     .tip()
     .attr("class", "d3-tip")
-    .html(function(d) {
+    .html(function (d) {
       let text =
         "<span style='color:white'>" + "Name: " + d.data.name + "</span><br>";
       text +=
@@ -44,6 +44,7 @@ Treemap.prototype.initVis = function() {
   // Graph title
   vis.svg
     .append("text")
+    .attr('class', 'vis-title')
     .attr("x", vis.svg.attr("width") / 2)
     .attr("y", 50)
     .attr("font-size", "30px")
@@ -53,13 +54,14 @@ Treemap.prototype.initVis = function() {
   // Graph sub-title
   vis.svg
     .append("text")
+    .attr('class', 'vis-subtitle')
     .attr("x", vis.svg.attr("width") / 2)
     .attr("y", 80)
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
     .text("Top 100 Most Sold Video Games Grouped by Platform");
 
-  vis.fader = function(color) {
+  vis.fader = function (color) {
     return d3.interpolateRgb(color, "#fff")(0.2);
   };
   vis.color = d3.scaleOrdinal(d3.schemeCategory20.map(vis.fader));
@@ -72,29 +74,37 @@ Treemap.prototype.initVis = function() {
     .round(true)
     .paddingInner(1);
 
+
+  vis.prepareDataForLayout(videoGameData);
+
+  vis.renderVis();
+};
+
+Treemap.prototype.prepareDataForLayout = function (data) {
+
+  let vis = this;
+
   // Since we are dealing with hierarchical data, need to convert the data to the right format
   vis.root = d3
-    .hierarchy(videoGameData)
-    .eachBefore(function(d) {
+    .hierarchy(data)
+    .eachBefore(function (d) {
       d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name;
     })
-    .sum(sumByValue)
-    .sort(function(a, b) {
+    .sum(vis.sumByValue)
+    .sort(function (a, b) {
       return b.height - a.height || b.value - a.value;
     });
 
   // Computes x0, x1, y0, and y1 for each node (where the rectangles should be)
   vis.treemap(vis.root);
-
-  // Return the size of the node
-  function sumByValue(d) {
-    return d.value;
-  }
-
-  vis.renderVis();
 };
 
-Treemap.prototype.renderVis = function() {
+// Return the size of the node
+Treemap.prototype.sumByValue = function (d) {
+  return d.value;
+};
+
+Treemap.prototype.renderVis = function () {
   let vis = this;
 
   vis.cell = vis.g
@@ -102,62 +112,145 @@ Treemap.prototype.renderVis = function() {
     .data(vis.root.leaves())
     .enter()
     .append("g")
-    .attr("transform", function(d) {
+    .attr("transform", function (d) {
       return "translate(" + d.x0 + "," + d.y0 + ")";
     });
 
   // Add rectanges for each of the boxes that were generated
-  let arr = [];
+  vis.arr = [];
   vis.cell
     .append("rect")
-    .attr("id", function(d) {
-      arr.push(d.data.category);
+    .attr("id", function (d) {
+      vis.arr.push(d.data.category);
       return d.data.id;
     })
-    .attr("width", function(d) {
+    .attr("width", function (d) {
       return d.x1 - d.x0;
     })
-    .attr("height", function(d) {
+    .attr("height", function (d) {
       return d.y1 - d.y0;
     })
-    .attr("fill", function(d) {
+    .attr("fill", function (d) {
       return vis.color(d.parent.data.id);
     })
     .on("mouseover", vis.tip.show)
     .on("mouseout", vis.tip.hide);
 
-  arr = arr.filter((v, i) => arr.indexOf(v) === i);
+  vis.arr = vis.arr.filter((v, i) => vis.arr.indexOf(v) === i);
 
   // Make sure that text labels don't overflow into adjacent boxes
   vis.cell
     .append("clipPath")
-    .attr("id", function(d) {
+    .attr("id", function (d) {
       return "clip-" + d.data.id;
     })
     .append("use")
-    .attr("xlink:href", function(d) {
+    .attr("xlink:href", function (d) {
       return "#" + d.data.id;
     });
 
   // Add text labels - each word goes on its own line
   vis.cell
     .append("text")
-    .attr("clip-path", function(d) {
+    .attr("clip-path", function (d) {
       return "url(#clip-" + d.data.id + ")";
     })
     .selectAll("tspan")
-    .data(function(d) {
+    .data(function (d) {
       return d.data.name.split(/(?=[A-Z][^A-Z])/g);
     })
     .enter()
     .append("tspan")
     .attr("x", 4)
-    .attr("y", function(d, i) {
+    .attr("y", function (d, i) {
       return 13 + i * 10;
     })
-    .text(function(d) {
+    .text(function (d) {
       return d;
     });
+
+  // Add an input to select between different summing methods
+  d3.selectAll("input")
+    .data(['videogames', 'movies'], function (d) {
+      return d ? d : this.value;
+    })
+    .on("change", changed);
+
+  function changed(categoryName) {
+
+    if (categoryName === 'videogames') {
+      vis.prepareDataForLayout(videoGameData);
+
+      vis.svg.select('.vis-title')
+        .text('Video Game Sales');
+      vis.svg.select('.vis-subtitle')
+        .text('Top 100 Most Sold Video Games Grouped by Platform');
+    } else {
+      vis.prepareDataForLayout(movieData);
+
+      vis.svg.select('.vis-title')
+        .text('Movie Sales');
+      vis.svg.select('.vis-subtitle')
+        .text('Top 100 Highest Grossing Movies Grouped By Genre');
+    }
+
+    let leaves = vis.root.leaves();
+
+    // Update the size and position of each of the rectangles
+    vis.cell.data(leaves);
+
+    vis.cell.transition().duration(750).attr("transform", function (d) {
+      return "translate(" + d.x0 + "," + d.y0 + ")";
+    })
+      .select("rect")
+      .attr("id", function (d) {
+        vis.arr.push(d.data.category);
+        return d.data.id;
+      })
+      .attr("width", function (d) {
+        return d.x1 - d.x0;
+      })
+      .attr("height", function (d) {
+        return d.y1 - d.y0;
+      })
+      .attr("fill", function (d) {
+        return vis.color(d.parent.data.id);
+      });
+
+    vis.cell
+      .select("clipPath")
+      .attr("id", function (d) {
+        return "clip-" + d.data.id;
+      })
+      .select("use")
+      .attr("xlink:href", function (d) {
+        return "#" + d.data.id;
+      });
+
+    vis.cell
+      .select("text")
+      .attr("clip-path", function (d) {
+        return "url(#clip-" + d.data.id + ")";
+      })
+      .selectAll("tspan")
+      .data(function (d) {
+        return d.data.name.split(/(?=[A-Z][^A-Z])/g);
+      })
+      .attr("x", 4)
+      .attr("y", function (d, i) {
+        return 13 + i * 10;
+      })
+      .text(function (d) {
+        return d;
+      });
+  }
+
+  vis.renderLegend();
+};
+
+Treemap.prototype.renderLegend = function () {
+
+  let vis = this;
 
   vis.legend = vis.g
     .append("g")
@@ -169,7 +262,7 @@ Treemap.prototype.renderVis = function() {
 
   let num = -1;
   let col = 0;
-  arr.forEach(function(platform) {
+  vis.arr.forEach(function (platform) {
     let legendRow;
     num++;
 
